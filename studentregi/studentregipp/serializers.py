961813e4d083a832f47c4965aed8b10a498d0119
souparna-navsoft <souparna.datta@navsoft.in>
2024-05-06 10:34:19 +0530

    Commit

--- studentregi/studentregipp/serializers.py
+++ studentregi/studentregipp/serializers.py
@@ -6,35 +6,5 @@ from django.contrib.auth import authenticate
 class StudentRegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = StudentRegister
-<<<<<<< HEAD
-        fields = ['id', 'username', 'first_name', 'last_name', 'age', 'gender', 'email', 'phone_number', 'password']
-        # extra_kwargs = {'password': {'write_only': True}}
-
-    # def create(self, validated_data):
-    #     user = StudentRegister.objects.create_user(**validated_data)
-    #     return user
-
-class StudentLoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField(write_only=True)
-    
-    def validate(self, attrs):
-        username = attrs.get('username')
-        password = attrs.get('password')
-
-        if username and password:
-            user = authenticate(username=username, password=password)
-            if not user:
-                raise serializers.ValidationError("Incorrect username or password.")
-
-            if not user.is_active:
-                raise serializers.ValidationError("User is inactive.")
-
-        else:
-            raise serializers.ValidationError("Both username and password are required.")
-
-        attrs['user'] = user
-        return attrs
-=======
-        fields = ['id' , 'first_name' , 'last_name' , 'age' , 'gender' , 'email' , 'phone_number' , 'password']
->>>>>>> souparna
+        fields = ['id', 'first_name', 'last_name', 'age', 'gender', 'email', 'phone_number', 'password']
+      
